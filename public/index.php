a2fe5a3638a193cfaddf3ad66fd03a5e9178e129
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2021-02-17 11:33:33 +0100

    Updated js dependencies Removed static stylesheet file

--- public/index.php
+++ public/index.php
@@ -21,9 +21,9 @@ require_once dirname(__DIR__) . '/bootstrap.php';
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/application.css" rel="stylesheet">
 </head>
-<body>
+<body style="background: #f4f4f4 url(images/pageBackground.png) repeat-x;">
 <div id="app">
     <div class="container">
         <a class="logo">
